a2a7b3d97bb20379cdf41b1cec894d566c72f874
Arthur Manyonge <arthurmanyonge22@gmail.com>
2024-09-21 14:26:23 +0300

    riverpod: create activity model

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,19 +1,1 @@
-import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Welcome Dripventory'),
-      ),
-      body: const Center(child: <Widget>[
-        Text(
-          'Welcome to Dripventory',
-        ),
-      ]),
-    );
-  }
-}
