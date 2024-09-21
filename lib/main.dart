84fcbcc8972f38df5734d57ad3337e76179824cd
Arthur Manyonge <arthurmanyonge22@gmail.com>
2024-09-21 14:54:13 +0300

    riverpod: consume created provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import './models/activity.dart';
+import './providers/provider.dart';
+
 void main() {
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(const ProviderScope(child: Home()));
 }
 
-class MyApp extends ConsumerWidget {
-  const MyApp({super.key});
+/// The homepage of our application
+class Home extends StatelessWidget {
+  const Home({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(title: const Text('Example')),
-      body: const Center(
-        child: Text(
-          'hello',
-        ),
-      ),
-    ));
+  Widget build(BuildContext context) {
+    return Consumer(
+      builder: (context, ref, child) {
+        // Read the activityProvider. This will start the network request
+        // if it wasn't already started.
+        // By using ref.watch, this widget will rebuild whenever the
+        // the activityProvider updates. This can happen when:
+        // - The response goes from "loading" to "data/error"
+        // - The request was refreshed
+        // - The result was modified locally (such as when performing side-effects)
+        // ...
+        final AsyncValue<Activity> activity = ref.watch(activityProvider);
+
+        return Center(
+          /// Since network-requests are asynchronous and can fail, we need to
+          /// handle both error and loading states. We can use pattern matching for this.
+          /// We could alternatively use `if (activity.isLoading) { ... } else if (...)`
+          child: switch (activity) {
+            AsyncData(:final value) => Text('Activity: ${value.activity}'),
+            AsyncError() => const Text('Oops, something unexpected happened'),
+            _ => const CircularProgressIndicator(),
+          },
+        );
+      },
+    );
   }
 }
